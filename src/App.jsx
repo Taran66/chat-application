7e4960838c761b45839881a4b2b9ea6267fcf64c
Taran66 <taranpreet1911@gmail.com>
2024-09-20 17:35:40 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import Container from "./components/Container";
 
 const App = () => 
 ( 
-      <div className="flex items-center justify-center w-screen h-screen bg-[url('./assets/background/beach.jpg')] bg-cover">
-          <div className="z-10 backdrop-filter backdrop-blur-xl bg-black/40 rounded-2xl shadow-2xl h-5/6 w-11/12 flex items-center justify-center">
+      <div className="flex items-center justify-center w-screen h-screen bg-[url('./assets/background/leaves3.jpg')] bg-cover">
+          <div className="z-10 backdrop-filter backdrop-blur-xl bg-white/30 rounded-2xl shadow-2xl h-5/6 w-11/12 flex items-center justify-center">
             <div className="w-11/12">
               <Container />
             </div>
--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -6,7 +6,7 @@ const Contacts = () => {
         <ul className="flex flex-col gap-2">
             {Users.map((user) => (
                 <li className="bg-black/70 rounded-lg h-16 flex items-center">
-                    <img src={user.profilePicture} alt="#" className="ml-4 h-12 rounded-full" key={user.id} />
+                    <img src={user.profilePicture} alt="#" className="ml-4 h-10 rounded-full" key={user.id} />
                     <div className="ml-6">
                         <h6 className="text-white">{user.username}</h6>
                         <p className="text-gray-400">{user.message}</p>
